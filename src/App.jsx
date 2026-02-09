549c027bc7758fd293eea7a543c62c09750b3901
User <user@example.com>
2026-02-09 10:20:46 +0100

    Full viewport mobile, no scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,7 @@ import React from 'react'
 import VideoPlayer from './components/VideoPlayer'
 
 function App() {
-  return (
-    <div style={{
-      width: '100%',
-      minHeight: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#000000'
-    }}>
-      <VideoPlayer />
-    </div>
-  )
+  return <VideoPlayer />
 }
 
 export default App
